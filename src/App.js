34ba886c8ec138071ab05938372722feffbf53ea
theoverse <theoverse@protonmail.com>
2022-03-04 08:20:56 -0600

    react component basics

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
+// JSX
 function App() {
   return (
     <div className="App">
---
+++ src/TestComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function TestComponent() {
+    return (
+        <div className='App'>
+            <h1>Test Component</h1>
+        </div>
+    )
+}
+
+export default TestComponent;

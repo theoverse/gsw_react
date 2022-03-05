97a7a006f9b84e6283f27097b256ceb65c6eace7
theoverse <theoverse@protonmail.com>
2022-03-05 12:58:31 -0600

    changing state with javascript event

--- src/App.js
+++ src/App.js
@@ -8,9 +8,6 @@ function App() {
   return (
     <div className="App">
       <h1>Theoverse</h1>
-      {/* <Book bookName="Title 1" writer="Author 1" />
-      <Book bookName="Title 2" writer="Author 2" />
-      <Book bookName="Title 3" writer="Author 3" /> */}
       <BookList />
     </div>
   );
--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -3,7 +3,6 @@ import Book from './Book'
 
 export class BookList extends Component {
 
-    // create state method 1
     state = {
         books: [
             { bookName: "Title 1", writer: "Author 1" },
@@ -12,16 +11,24 @@ export class BookList extends Component {
         ]
     }
 
-    // create state method 2
-    // constructor() {
-    //     super();
-    //     this.state = {}
-    // }
+    changeBookState = () => {
+        // console.log("Clicked")
+        this.setState({
+            books: [
+                { bookName: "New Title 1", writer: "Author 1" },
+                { bookName: "New Title 2", writer: "Author 2" },
+                { bookName: "New Title 3", writer: "Author 3" },
+            ]
+        })
+    }
 
     render() {
         return (
             <div className="App">
                 <h1>Book List</h1>
+
+                <button onClick={this.changeBookState}>Change State</button>
+
                 <Book bookName={this.state.books[0].bookName} writer={this.state.books[0].writer} />
                 <Book bookName={this.state.books[1].bookName} writer={this.state.books[1].writer} />
                 <Book bookName={this.state.books[2].bookName} writer={this.state.books[2].writer} />

0a52b135509feeeac5dc174c30bd2cfe54d6056e
theoverse <theoverse@protonmail.com>
2022-03-05 13:09:01 -0600

    changing state with input elements

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -22,12 +22,24 @@ export class BookList extends Component {
         })
     }
 
+    changeInput = e => {
+        this.setState({
+            books: [
+                { bookName: e.target.value, writer: "Author 1" },
+                { bookName: "New Title 2", writer: "Author 2" },
+                { bookName: "New Title 3", writer: "Author 3" },
+            ]
+        })
+    }
+
     render() {
         return (
             <div className="App">
                 <h1>Book List</h1>
 
                 <button onClick={this.changeBookState}>Change State</button>
+
+                <input type="text" onChange={this.changeInput} />
 
                 <Book bookName={this.state.books[0].bookName} writer={this.state.books[0].writer} />
                 <Book bookName={this.state.books[1].bookName} writer={this.state.books[1].writer} />
